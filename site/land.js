24342d946d44c438fcf414835645941d751f4c90
wombat1742 <tomgogard@gmail.com>
2024-09-28 19:42:52 +0300

    added toasts

--- site/land.js
+++ site/land.js
@@ -17,5 +17,21 @@ function submitData(e) {
     data.telegram = form.telegram.value;
     axios
         .post('/create-request', data)
-        .then(response => console.log(response))
+        .then(response =>{
+            console.log(response);
+            launch_success_toast();
+        })
+        .catch(error => launch_error_toast())
+}
+var error = document.getElementById("error-toast")
+var success = document.getElementById("success-toast")
+function launch_success_toast() {
+    
+    success.className = "toast show";
+    setTimeout(function(){ success.className = success.className.replace("show", ""); }, 5000);
+}
+function launch_error_toast() {
+    
+    error.className = "toast show";
+    setTimeout(function(){ error.className = error.className.replace("show", ""); }, 5000);
 }
